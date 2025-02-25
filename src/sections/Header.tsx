8b18e104414a3aa0714d040b8c522d15896729fe
samuelhorjet <samuelhorjet001@gmail.com>
2025-02-25 18:07:18 +0100

    Initial commit

---
+++ src/sections/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="text-white">
+      <p>get started for free</p>
+    </div>
+  );
+};
+
+export default Header;
